0ef613c38ef6192e2123c8ebc4f8467cfadb1288
unknown <mhm1404@naver.com>
2025-12-12 16:34:09 +0900

    수정2

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "최신 Docker 이미지를 가져오는 중..."
-docker-compose pull
+docker-compose -f docker-compose.remote.yml pull
 
 echo "서비스 재시작 중..."
-docker-compose up -d
+docker-compose -f docker-compose.remote.yml up -d
 
 echo "완료! http://localhost:5173 에서 확인하세요"
